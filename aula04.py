3b717f86f56234e496e38deeaf97c02f16790c47
João Bianchi <127564437+Jbianchi182@users.noreply.github.com>
2025-05-19 23:12:57 -0300

    upload do exercicio da aula 04

---
+++ aula04.py
@@ -0,0 +1,12 @@
+nome = input('Qual é o seu nome? ')
+print(f'O seu nome é {nome}')
+
+print('---------------')
+
+numero1 = input('Digite um número: ')
+numero2 = input('Digite outro número: ')
+
+int_num1 = int(numero1)
+int_num2 = int(numero2)
+
+print(f'A soma dos números é: {int_num1+int_num2}')
