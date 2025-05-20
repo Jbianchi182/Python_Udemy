e3ecff3881447cdaf5fe4f331d9a5054bfa09824
João Bianchi <127564437+Jbianchi182@users.noreply.github.com>
2025-05-19 23:11:29 -0300

    upload do exercicio da aula 01

---
+++ aula01.py
@@ -0,0 +1,13 @@
+nome = 'Joao'
+sobrenome = 'Bianchi'
+idade = 30
+ano_nasc = 1993
+maior_idade = idade > 17
+altura = 1.79
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nasc)
+print('É maior de idade: ', maior_idade)
+print('Altura em metros: ', altura)
